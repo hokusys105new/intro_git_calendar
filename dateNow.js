080926221bbf030ef40a0167ebac03cd7c909b21
hokusys105 <hokusys105new@hokusys.jp>
2024-04-22 10:08:21 +0900

    first commit

---
+++ dateNow.js
@@ -0,0 +1,12 @@
+// 現在の日付を取得
+const today = new Date();
+const month = today.getMonth() + 1;
+const day = today.getDate();
+
+// 季節を計算
+let season = "";
+
+
+// 結果をHTMLに表示
+const seasonText = document.getElementById("date");
+seasonText.textContent = `${month}月${day}日 ${season}`;
